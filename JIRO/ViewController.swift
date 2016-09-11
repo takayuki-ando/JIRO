acbb54f6a052f7190279e30ee9c493d3dad40b43
takayukiyuki <takayuki.ando0403@gmail.com>
2016-09-11 10:04:53 +0900

    Initial Commit

---
+++ JIRO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JIRO
+//
+//  Created by 安藤隆行 on 2016/09/11.
+//  Copyright © 2016年 安藤隆行. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JIROTests/JIROTests.swift
@@ -0,0 +1,36 @@
+//
+//  JIROTests.swift
+//  JIROTests
+//
+//  Created by 安藤隆行 on 2016/09/11.
+//  Copyright © 2016年 安藤隆行. All rights reserved.
+//
+
+import XCTest
+@testable import JIRO
+
+class JIROTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
